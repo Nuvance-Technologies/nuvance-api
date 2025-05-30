4709fe4973f6adebab8176220476c575b28713c7
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-05-30 17:29:59 +0530

    added fe url, cors issue fixed

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ app.use(express.json());
 const corsOptions = {
     origin: [
         'http://localhost:3000',
+        'https://nuvance-demo.vercel.app/',
         'http://ec2-13-60-23-35.eu-north-1.compute.amazonaws.com:3000',
         'https://nuvance-portfolio.vercel.app'
     ],
